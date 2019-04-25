7c3ee564652474aadef27ee6b8a1ab0b616c5a41
Mehrnoush504 <Mehrnoush@gmail.com>
2019-04-25 05:27:35 +0430

    Update J_Type.py

--- assembler/J_Type.py
+++ assembler/J_Type.py
@@ -1,5 +1,8 @@
+import sys
+import decimal_and_binary_converter as db
+
+
 class JType:
-    instructions = dict()
     op = ''
     target = ''
     instructions = {'j': '1101', 'halt': '1110'}
@@ -8,33 +11,36 @@ class JType:
         pass
 
     def calc(self, line, symbol_table):
-        linecontent=line.splite()
-        flag=False
-        index=-1
-        for key in self.instructions.keys():
-            if linecontent[0]==key:
-                op=self.instructions[key]
-                index=1
-                flag=True
-                break
-            if not flag:
-                for key in self.instructions.keys():
-                    if linecontent[1]==key:
-                        op=self.instructions[key]
-                        index=2
-                        break
-        if op=='j':
-            for key in symbol_table.keys():
-                if linecontent[index]==key:
-                    target+=str(desimal_to_inary(symbol_table[key]))
-                    if len(target)>16:
-                        sys.exit('target out of range')
-                    zero=''
-                    for i in range(0,16-len(target)):
-                        zero+='0'
-                    target=zero+ target
-                    break
-        elif op='halt':
-            target='0000000000000000'
+        line_content = line.split()
 
-        return'0000'+op+'00000000'+target
+        flag = False
+        index = -1
+        for key in self.instructions.keys():
+            if line_content[0] == key:
+                self.op = self.instructions[key]
+                index = 1
+                flag = True
+                break
+
+        if not flag:
+            for key in self.instructions.keys():
+                if line_content[1] == key:
+                    self.op = self.instructions[key]
+                    index = 2
+                    break
+
+        if self.op == '1101':  # j
+            for key in symbol_table.keys():
+                if line_content[index] == key:
+                    self.target += str(db.decimal_to_binary(symbol_table[key]))
+                    if len(self.target) > 16:
+                        sys.exit('target out of range')
+                    zero = ''
+                    for i in range(0, 16 - len(self.target)):
+                        zero += '0'
+                    self.target = zero + self.target
+                    break
+        elif self.op == '1110':
+            self.target = '0000000000000000'
+
+        return '0000' + self.op + '00000000' + self.target
