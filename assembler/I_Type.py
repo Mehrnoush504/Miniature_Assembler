2f16797375c34130a8b7a75cb580c81c6c81a3b8
Mehrnoush504 <Mehrnoush@gmail.com>
2019-04-25 05:29:07 +0430

    Update I_Type.py

--- assembler/I_Type.py
+++ assembler/I_Type.py
@@ -1,6 +1,11 @@
+import sys
+import decimal_and_binary_converter as db
+import two_complement as towComp
+
+
 class IType:
-    instructions = dict()
-    instructions ={'addi':'0101' , 'ori':'0111' , 'slti':'0110' , 'lui':'1000' , 'lw':'1001' , 'sw':'1010' , 'beq':'1011' , 'jalr':'1100'}
+    instructions = {'addi': '0101', 'ori': '0111', 'slti': '0110', 'lui': '1000', 'lw': '1001', 'sw': '1010',
+                    'beq': '1011', 'jalr': '1100'}
     op = ''
     rs = ''
     rt = ''
@@ -9,117 +14,141 @@ class IType:
     def __init__(self):
         pass
 
-    def calc(self, line, pc, symbol_table):
-        linecontent=line.split()
-        flag=False
-        index=-1
-        print linecontent
+    def calc(self, line, pc, symbol_table, registers):
+        print('i type ', line)
+        line_content = line.split()
+        flag = False
+        index = -1
+
         for key in self.instructions.keys():
-            if linecontent[0]==key:
-                op=self.instructions[key]
-                index=1
-                flag=True
+            if line_content[0] == key:
+                self.op = self.instructions[key]
+                index = 1
+                flag = True
                 break
-        if flag==False:
+
+        if not flag:
             for key in self.instructions.keys():
-                if linecontent[1]==key:
-                    op=self.instructions[key]
-                    index=2
+                if line_content[1] == key:
+                    self.op = self.instructions[key]
+                    index = 2
                     break
-        
-        fields=linecontent[index].split(',')
 
-        if linecontent[index-1]=='lui':   
-            found =False
+        fields = line_content[index].split(',')
+
+        if line_content[index - 1] == 'lui':
+            found = False
             for key in registers.keys():
-                if fields[0]==key:
-                    rt=registers[key]
-                    found=True
+                f = int(fields[0])
+                if f == key:
+                    self.rt = registers[key]
+                    found = True
                     break
-            if found==False:
+
+            if not found:
                 sys.exit('I_type:rt not found!')
+
             for key in symbol_table.keys():
-                if fields[1]==key:
-                    imm=str(decimal_to_binary(symbol_table[key]))
-            if len(imm)>16:
+                if fields[1] == key:
+                    self.imm = str(db.decimal_to_binary(symbol_table[key]))
+            if len(self.imm) > 16:
                 sys.exit('I_type:imm out of range!')
-            zero=''
-            for i in range (0,16-len(imm)):
-                zero+='0'
-            imm=zero+imm
-        elif linecontent[index-1]=='jalr':
-            found =False
-            for key in registers.keys():
-                if fields[0]==key:
-                   rt=registers[key]
-                   found=True
-                   break
-            if found==False:
-                sys.exit('I_type:rt not found!')
-            
-            found =False
-            for key in registers.keys():
-                if fields[1]==key:
-                   rs=registers[key]
-                   found=True
-                   break
-            if found==False:
-                sys.exit('I_type:rs not found!')
-            
-            for i n range(0,16):
-                imm+='0'
-        else:
-            found =False
-            for key in registers.keys():
-                if fields[0]==key:
-                   rt=registers[key]
-                   found=True
-                   break
-            if found==False:
-                sys.exit('I_type:rt not found!')
-            
-            found =False
-            for key in registers.keys():
-                if fields[1]==key:
-                   rs=registers[key]
-                   found=True
-                   break
-            if found==False:
-                sys.exit('I_type:rs not found!')
-            
-            #az inja be payin chek shavad
-            flage=False
-            for key in symbol_table.keys():
-                if fields[2]==key:
-                    imm=str(desimal_to_bainary(symbol_table[key]))
-                    if len(imm)>16:
-                        sys.exit('I_type:imm out of range!')
-                    zero=''
-                    for i in range(0,16-len(imm)):
-                        zero+='0'
-                    imm + = zero
-                    flag=True
-                
-                if flage==False and not isdigit(fields[2]):
-                    sys.exit('I_type: incorect imm')
-                
-                elif isdigit(fields[2]):
-                    num=int (fields[2])
-                    imm=str(decimal_to_binary(num))
-                    if len(imm)>16:
-                        sys.exit('I_type:imm out of range!')
-                    zero=''
-                    for i in range(0,16-len(imm)):
-                        zero+='0'
-                    imm + = zero
-                
-            if linecontent[index-1] == 'beq':
-                    lable=-1
-                    lable=int(imm)
-                    if lable-pc-1>= 0:
-                        imm=str(lable-pc-1)
-                
-            else:
-                imm=two_comp(lable-pc-1)
+            zero = ''
+            for i in range(0, 16 - len(self.imm)):
+                zero += '0'
+            self.imm = zero + self.imm
 
-        return '0000' + op + rs + rt + imm
+        elif line_content[index - 1] == 'jalr':
+            found = False
+            for key in registers.keys():
+                f = int(fields[0])
+                if f == key:
+                    self.rt = registers[key]
+                    found = True
+                    break
+
+            if not found:
+                sys.exit('I_type:rt not found!')
+
+            found = False
+            for key in registers.keys():
+                f = int(fields[1])
+                if f == key:
+                    self.rs = registers[key]
+                    found = True
+                    break
+
+            if not found:
+                sys.exit('I_type:rs not found!')
+
+            for i in range(0, 16):
+                self.imm += '0'
+        else:
+            found = False
+            for key in registers.keys():
+                f = int(fields[0])
+                if f == key:
+                    self.rt = registers[key]
+                    found = True
+                    break
+
+            if not found:
+                sys.exit('I_type:rt not found!')
+
+            found = False
+            for key in registers.keys():
+                f = int(fields[1])
+                if f == key:
+                    self.rs = registers[key]
+                    found = True
+                    break
+
+            if not found:
+                sys.exit('I_type:rs not found!')
+
+            # az inja be payin chek shavad
+            not_in_symbols = True
+            for key in symbol_table.keys():
+                if fields[2] == key:
+                    imm = str(db.decimal_to_binary(symbol_table[key]))
+                    if len(imm) > 16:
+                        sys.exit('I_type:imm out of range!')
+                    zero = ''
+                    for i in range(0, 16 - len(imm)):
+                        zero += '0'
+                    self.imm = zero + imm
+                    print('after symbol', self.imm)
+                    not_in_symbols = False
+
+            not_in_numbers = True
+            if fields[2].isdigit():
+                num = int(fields[2])
+                imm = str(db.decimal_to_binary(num))
+                if len(imm) > 16:
+                    sys.exit('I_type:imm out of range!')
+                zero = ''
+                for i in range(0, 16 - len(imm)):
+                    zero += '0'
+                self.imm = zero + imm
+                print('after digit', self.imm)
+                not_in_numbers = False
+            in_beq = False
+            if line_content[index - 1] == 'beq':
+                label = db.binary_to_decimal(self.imm)
+                if label - pc - 1 >= 0:
+                    imm = str(db.decimal_to_binary(label - pc - 1))
+                    if len(imm)>16:
+                        sys.exit('i type: in beq imm out of bound')
+                    zero = ''
+                    for i in range(0, 16-len(imm)):
+                        zero += '0'
+                    self.imm = zero + imm
+                    in_beq = True
+                else:
+                    self.imm = towComp.tow_comp(label - pc - 1)
+                    in_beq = True
+
+            if not in_beq:
+                if not_in_symbols and not_in_numbers:
+                    sys.exit('I_type: imm not found!')
+        return '0000' + self.op + self.rs + self.rt + self.imm
