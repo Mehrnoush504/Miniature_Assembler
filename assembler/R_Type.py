af1cc823997d38b864fdc1ef301f8d9c3b19242f
Mehrnoush504 <Mehrnoush@gmail.com>
2019-04-25 05:31:52 +0430

    Update R_Type.py

--- assembler/R_Type.py
+++ assembler/R_Type.py
@@ -2,7 +2,6 @@ import sys
 
 
 class RType:
-    instructions = dict()
     op = ''
     rs = ''
     rt = ''
@@ -13,35 +12,39 @@ class RType:
         pass
 
     def calc(self, line, registers):
-        linecontent = line.split()
+        line_content = line.split()
         flag = False
         index = -1
         for key in self.instructions.keys():
-            if linecontent[0] == key:
+            if line_content[0] == key:
                 op = self.instructions[key]
                 flag = True
                 index = 1
                 break
+
         if not flag:
             for key in self.instructions.keys():
-                if linecontent[1] == key:
+                if line_content[1] == key:
                     op = self.instructions[key]
-                    flag = True
                     index = 2
                     break
-        fields = linecontent[index].split(',')
+
+        fields = line_content[index].split(',')
         found = False
         for key in registers.keys():
-            if fields[0] == key:
+            f = int(fields[0])
+            if f == key:
                 rd = registers[key]
                 found = True
                 break
+
         if not found:
             sys.exit('R_type : rd not found')
 
         found = False
         for key in registers.keys():
-            if fields[1] == key:
+            f = int(fields[1])
+            if f == key:
                 rs = registers[key]
                 found = True
                 break
@@ -50,7 +53,8 @@ class RType:
 
         found = False
         for key in registers.keys():
-            if fields[2] == key:
+            f = int(fields[2])
+            if f == key:
                 rt = registers[key]
                 found = True
                 break
@@ -58,3 +62,5 @@ class RType:
             sys.exit('R_type : rt not found')
 
         return '0000' + op + rs + rt + rd + '000000000000'
+
+
